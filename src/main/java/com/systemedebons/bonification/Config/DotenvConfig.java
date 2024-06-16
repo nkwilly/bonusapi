e86721c32d5a078099eba6caace5796a89c764c2
Momo-azangue <azanguewill@gmail.com>
2024-06-16 23:31:21 +0200

    modification sur le dotenv

--- src/main/java/com/systemedebons/bonification/Config/DotenvConfig.java
+++ src/main/java/com/systemedebons/bonification/Config/DotenvConfig.java
@@ -7,12 +7,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DotenvConfig {
 
-    @PostConstruct
+  /***  @PostConstruct
     public void init() {
         Dotenv dotenv = Dotenv.load();
         dotenv.entries().forEach((entry) -> {
             System.setProperty(entry.getKey(), entry.getValue());
         });
+    }**/
+
+    @PostConstruct
+    public void init() {
+        Dotenv dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load();
+
+        System.setProperty("SPRING_MAIL_HOST", dotenv.get("SPRING_MAIL_HOST", "smtp.gmail.com"));
+        System.setProperty("SPRING_MAIL_PORT", dotenv.get("SPRING_MAIL_PORT", "587"));
+        System.setProperty("SPRING_MAIL_USERNAME", dotenv.get("SPRING_MAIL_USERNAME", "azanguewill@gmail.com"));
+        System.setProperty("SPRING_MAIL_PASSWORD", dotenv.get("SPRING_MAIL_PASSWORD", "joig fabk well fhtg"));
     }
 
 }

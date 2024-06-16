2b300cf0d86f91827a9974ee6f688abdb14dc078
Momo-azangue <azanguewill@gmail.com>
2024-06-17 01:49:36 +0200

    add new link

--- src/main/java/com/systemedebons/bonification/Security/WebSecurityConfig.java
+++ src/main/java/com/systemedebons/bonification/Security/WebSecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +29,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     @Autowired

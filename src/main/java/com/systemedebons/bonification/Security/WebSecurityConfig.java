46515041e1e448a5e10f569d17632c4fb27956c3
Willy Watcho <107815299+nkwilly@users.noreply.github.com>
2025-03-03 18:24:21 +0100

    Update WebSecurityConfig.java

--- src/main/java/com/systemedebons/bonification/Security/WebSecurityConfig.java
+++ src/main/java/com/systemedebons/bonification/Security/WebSecurityConfig.java
@@ -88,7 +88,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://bonus-front-mu.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList("https://bonus-front-mu.vercel.app", "http://documentation-bonus.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
